a832b148cb5c22d67ed27f6d998d35c537197ce9
PI3RRE003 <vitorcruz.dev@gmail.com>
2025-08-02 20:20:58 -0300

    feat: adicionando testes

--- app.py
+++ app.py
@@ -33,7 +33,7 @@ def create_task():
     task_id_control +=1
     tasks.append(new_task)
     print(tasks)
-    return jsonify({"message":"Nova tarefa criada com sucesso"})
+    return jsonify({"message":"Nova tarefa criada com sucesso", "id": new_task.id})
 #CHAMA TODA LISTA DE TAREFAS
 @app.route('/tasks', methods=['GET'])
 def get_tasks():
@@ -77,7 +77,7 @@ def delete_task(id):
         if t.id == id:
             print(t)
             task = t
-            break#caso não tenha break ele percorrera o laço mesmo ja tendo encontrado
+            break#caso não tenha break ele percorrera o laço mesmo ja tendo ENCONTRADO
 
     if task == None:
         return jsonify({"message":"Não foi possivel encontrar a atividade"}), 404
